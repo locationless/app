639d637a96519265e165d097bc888edde1a727e5
Julian Fell <julesfell@gmail.com>
2018-12-11 19:36:46 +1000

    Updates

---
+++ precache-manifest.50199ad571ba2ee8fdbf5e79a60192b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "945a8ec5c43fe19a8986",
+    "url": "/static/js/main.945a8ec5.chunk.js"
+  },
+  {
+    "revision": "2a25cf63ae122dc5d0ca",
+    "url": "/static/js/1.2a25cf63.chunk.js"
+  },
+  {
+    "revision": "945a8ec5c43fe19a8986",
+    "url": "/static/css/main.ed1e626c.chunk.css"
+  },
+  {
+    "revision": "2a25cf63ae122dc5d0ca",
+    "url": "/static/css/1.8a59af20.chunk.css"
+  },
+  {
+    "revision": "f8e6530e5e90cbd46ad1bb78e75cf445",
+    "url": "/index.html"
+  }
+];
