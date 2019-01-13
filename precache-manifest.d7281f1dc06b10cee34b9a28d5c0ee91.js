a8c6cfc39b18d3d720fb40e444377bb8392842dc
Julian Fell <julesfell@gmail.com>
2019-01-13 14:38:19 +1000

    Updates

---
+++ precache-manifest.d7281f1dc06b10cee34b9a28d5c0ee91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c9a6909472c311b7804",
+    "url": "/static/js/main.5c9a6909.chunk.js"
+  },
+  {
+    "revision": "d7cb74900d63f54e5a99",
+    "url": "/static/js/1.d7cb7490.chunk.js"
+  },
+  {
+    "revision": "5c9a6909472c311b7804",
+    "url": "/static/css/main.faed6442.chunk.css"
+  },
+  {
+    "revision": "d7cb74900d63f54e5a99",
+    "url": "/static/css/1.3b3dae7e.chunk.css"
+  },
+  {
+    "revision": "13af2d8b216cd6c89f4cd4bccd128b5b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c462c1e277dae04f67df4c0f2c80a6a3.js"
+  "/precache-manifest.d7281f1dc06b10cee34b9a28d5c0ee91.js"
 );
 
 workbox.clientsClaim();
